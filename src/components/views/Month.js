e7b04341157fbdcb1011f6b7984750a6873a0e45
Mustafa <mustafa.mamun.khondkar@ericsson.com>
2020-03-16 11:19:59 +0200

    acorn version bump

--- src/components/views/Month.js
+++ src/components/views/Month.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react';
 import { eachDayOfInterval } from 'date-fns';
+import { Grid, GridColumn, GridRow } from 'semantic-ui-react';
 
 import { CalContext } from '../../context/Context';
+import { getDate } from 'date-fns/esm';
 
 const Month = () => {
   const { viewWindow } = useContext(CalContext);
-  console.log(
-    eachDayOfInterval({ start: viewWindow.start, end: viewWindow.end })
+  const eachDay = eachDayOfInterval({
+    start: viewWindow.start,
+    end: viewWindow.end
+  });
+  return (
+    <Grid columns={7}>
+      <GridRow>
+        {eachDay.map(day => {
+          const date = getDate(day);
+          return <GridColumn>{date}</GridColumn>;
+        })}
+      </GridRow>
+    </Grid>
   );
-  return <div>Month</div>;
 };
 
 export default Month;
